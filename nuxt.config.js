785313d1700c3f152f07b11b115f9d847f0c8212
Danila <da_buddha@bk.ru>
2022-05-10 00:05:25 +0300

    loader for ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,10 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'https://libretranslate.de/',
   },
-
+  loading: {
+    color: '#1890ff',
+    height: '3px'
+  },
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
